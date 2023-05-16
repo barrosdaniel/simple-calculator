102fa450e7a1b52a4c40a50547191e5780e6294d
barrosdaniel <barros.daniel@outlook.com>
2023-05-16 22:59:21 +0800

    feature: Add calculator functionality.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿
-using System;
+﻿using System;
 
 namespace SimpleCalculator
 {
@@ -7,7 +6,33 @@ namespace SimpleCalculator
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("=== Simple Calculator ===");
+            Console.WriteLine("Enter the first operand: ");
+            double number_1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the operation [+, -, *, /]: ");
+            string operation = Console.ReadLine();
+            Console.WriteLine("Enter the second operand: ");
+            double number_2 = Convert.ToDouble(Console.ReadLine());
+            double result = 0;
+            switch (operation)
+            {
+                case "+":
+                    result = number_1 + number_2;
+                    break;
+                case "-":
+                    result = number_1 - number_2;
+                    break;
+                case "*":
+                    result = number_1 * number_2;
+                    break;
+                case "/":
+                    result = number_1 / number_2;
+                    break;
+                default:
+                    Console.WriteLine("Invalid operation!");
+                    break;
+            }
+            Console.WriteLine("Result: {0}", result);
         }
     }
 }
